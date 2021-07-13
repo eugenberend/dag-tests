b2c58df3ed6235d4358779f5340c04e45e98915e
eugenberend <evgeniy.berendyaev@gmail.com>
2021-07-13 14:04:38 +0300

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,6 +1,6 @@
 import prefect
 from prefect import task, Flow
-
+from prefect.storage import GitHub
 @task
 def say_hello():
     logger = prefect.context.get("logger")
@@ -8,3 +8,11 @@ def say_hello():
 
 with Flow("hello-flow") as flow:
     say_hello()
+
+flow.storage = GitHub(
+    repo="eugenberend/dag-tests",                           # name of repo
+    path="example1.py"                   # location of flow file in repo
+    #access_token_secret="GITHUB_ACCESS_TOKEN"  # name of personal access token secret
+)
+
+flow.register(project_name="Tutorial",labels=["common-k8s"])
